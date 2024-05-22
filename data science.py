010b8554a1a02fd13f117ca5e86617f718d372df
waelak1 <mhd.abokalam@bahcesehir.edu.tr>
2024-05-22 23:06:09 +0300

    Add files via upload

---
+++ data science.py
@@ -0,0 +1,50 @@
+# Data Science Final Project
+## Introduction
+This notebook contains the final project for the Data Science course. The project involves various exercises to demonstrate the use of markdown and code cells in Jupyter notebooks.
+## Data Science Languages
+Some of the popular languages used in Data Science are:
+- Python
+- R
+- SQL
+- Julia
+- Java
+- Scala
+## Data Science Libraries
+Popular libraries used in Data Science include:
+- Pandas
+- NumPy
+- Matplotlib
+- Seaborn
+- Scikit-learn
+- TensorFlow
+- Keras
+- PyTorch
+## Data Science Tools
+| Tool          | Description                                  |
+|---------------|----------------------------------------------|
+| Jupyter       | Interactive computing environment           |
+| RStudio       | Integrated development environment for R    |
+| Apache Spark  | Cluster-computing framework                 |
+| TensorFlow    | Machine learning framework                  |
+| RapidMiner    | Data science platform for analytics         |
+## Arithmetic Expression Examples
+Below are examples of arithmetic operations like addition and multiplication.
+# Multiplication and addition
+a = 5
+b = 4
+multiply = a * b
+add = a + b
+multiply, add
+# Convert minutes to hours
+minutes = 150
+hours = minutes / 60
+hours
+## Objectives
+The objectives of this notebook are:
+- To demonstrate the use of markdown cells.
+- To list common data science languages and libraries.
+- To showcase arithmetic operations using code cells.
+- To convert time units using code cells.
+- To create a table of commonly used data science tools.
+## Author
+This notebook was created by ['mhd wael abo kalam'].
